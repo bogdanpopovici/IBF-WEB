3d580dfd0b68371a2dc2451240db61ad437a4b14
Bogdan Popovici <pbp1g12@soton.ac.uk>
2015-11-13 00:09:38 +0000

    project seetup

--- core/settings.py
+++ core/settings.py
@@ -97,6 +97,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 
 import dj_database_url
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
